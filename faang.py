64e470b4b00c56468913a56f021dadc1b83dc6b1
Svitlana <G00473000@atu.ie>
2025-12-20 20:55:15 +0000

    update faang.py

--- faang.py
+++ faang.py
@@ -3,10 +3,11 @@
 
 
 # Import necessary libraries.
-import datetime
+import datetime as dt
 import os
 import pandas as pd
 import matplotlib.pyplot as plt 
+
 
 
 
@@ -16,8 +17,6 @@ def plot_data():
     Reads the latest CSV from 'data', plots Close prices of all stocks,
     and saves the plot in 'plots' folder with timestamp.
     """
-
-    now = datetime.datetime.now()
 
     # Get all CSV files in the data folder
     files = [f for f in os.listdir('data') if f.endswith('.csv')]
@@ -30,9 +29,12 @@ def plot_data():
     # Create a figure
     plt.figure(figsize=(10,6))
 
-    # Plot each stock Close column
-    for stock in df['Close'].columns:
-        plt.plot(df.index, df['Close'][stock], label=stock)
+    # Plot Close prices (explicitly for clarity)
+    plt.plot(df.index, df["Close"]["META"], label="META")
+    plt.plot(df.index, df["Close"]["AAPL"], label="AAPL")
+    plt.plot(df.index, df["Close"]["AMZN"], label="AMZN")
+    plt.plot(df.index, df["Close"]["NFLX"], label="NFLX")
+    plt.plot(df.index, df["Close"]["GOOG"], label="GOOG")
 
     # Labels, title, legend
     plt.xlabel("Date")
@@ -40,10 +42,11 @@ def plot_data():
     plt.title(f"Stock Prices as of {df.index[-1].date()}")
     plt.legend()
 
-    # Save plot.
-    file_name = now.strftime("%Y%m%d-%H%M%S") + ".png"
-    plt.savefig(file_name)
+    # Plotting.
+    timestamp = dt.datetime.now().strftime("%Y%m%d-%H%M%S")
+    plt.savefig("plots/" + timestamp + ".png")
     plt.close()
+    
 
 # This line means that the following code will only run if this script is executed directly, it wont run if this script is imported as a module in another script.
 if __name__ == "__main__":
